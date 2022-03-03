a42dae6035ecfdab984996d76b86c099db182cba
Gaurav Pandey <38068051+gauravhacky@users.noreply.github.com>
2022-03-03 22:58:31 +0530

    Add files via upload

---
+++ Model/CountdownTimer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CountdownTimer extends Model
+{
+    use SoftDeletes;
+    use HasFactory;
+}
